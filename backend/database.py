8ae14c342c5e8a117281722a9d8b9612949f106f
Asutosh Sahoo <toshasu56@gmail.com>
2026-06-29 18:29:28 +0530

    Fix CSV filenames and update database import

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
-import os
+import pandas as pd
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://ecommerce_analytics_1kdt_user:YYUU9eOTLjSBqNqXNkaiBlYBkb5n9nsF@dpg-d8vnssjtqb8s73f15q4g-a.oregon-postgres.render.com/ecommerce_analytics_1kdt"
 
 engine = create_engine(DATABASE_URL)
--- backend/import_data.py
+++ backend/import_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Paste your Render External Database URL below
+DATABASE_URL = "postgresql://ecommerce_analytics_1kdt_user:YYUU9eOTLjSBqNqXNkaiBlYBkb5n9nsF@dpg-d8vnssjtqb8s73f15q4g-a.oregon-postgres.render.com/ecommerce_analytics_1kdt"
 
 engine = create_engine(DATABASE_URL)
 
